92daf0a0338bd4bdcfdf2530913997ee458130ea
Alex Cibotari <alexandru.cibotari@gmail.com>
2020-01-19 21:12:04 +0100

    add Instructor in search filter

--- src/app/shared/pipe/filter.pipe.ts
+++ src/app/shared/pipe/filter.pipe.ts
@@ -5,7 +5,11 @@ import {Course} from '../model/course.model';
 export class FilterPipe implements PipeTransform {
   transform(items: Course[], filter: string): Course[] {
     if (filter != null || filter !== '') {
-      return items.filter(it => it.title.toLocaleLowerCase().indexOf(filter.toLocaleLowerCase()) > -1);
+      return items.filter(course => {
+        return course.title.toLocaleLowerCase().indexOf(filter.toLocaleLowerCase()) > -1 ||
+            course.visible_instructors
+            .some(instructor => instructor.display_name.toLocaleLowerCase().indexOf(filter.toLocaleLowerCase()) > -1);
+      });
     }
     return items;
   }
